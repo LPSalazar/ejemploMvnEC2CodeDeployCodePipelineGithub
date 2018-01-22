9ef49e1b6bf56e3b65020afcb1bb6628f3e32294
LPSalazar <luisp.wtow@gmail.com>
2018-01-22 17:26:00 -0300

    desde ecplise

---
+++ ejemplo1/src/main/java/practica/mvnEC2CodeDeployCodePipelineGithub/ejemplo1/CreadorDeArchivos.java
@@ -0,0 +1,28 @@
+package practica.mvnEC2CodeDeployCodePipelineGithub.ejemplo1;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CreadorDeArchivos {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		String ruta="/home/ec2-user/ejemploMvnEC2CodeDeployCodePipelineGithub/ejemploDefinitivo.txt";
+		File archivo= new File(ruta);
+		BufferedWriter bw;
+		if(!archivo.exists()) {
+			bw= new BufferedWriter(new FileWriter(archivo));
+			bw.write("Funciona perfectamente, se genero un proyecto maven el cual se ejecuta "
+					+ "por medio del codeDeploy y codePipeline. Ademas se encuentra almacenado en Github");	
+		}
+		else {
+			bw= new BufferedWriter(new FileWriter(archivo));
+			bw.write("esto ya estaba creado, pero seguramente se hizo una modificacion");
+		}
+		bw.close();
+	}
+
+}
