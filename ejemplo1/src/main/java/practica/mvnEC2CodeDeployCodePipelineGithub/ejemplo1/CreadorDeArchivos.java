3a041a14ce520f15ae041c39a457a18bc6858e2e
LPSalazar <luisp.wtow@gmail.com>
2018-01-24 13:53:28 -0300

    dgdgdg

--- ejemplo1/src/main/java/practica/mvnEC2CodeDeployCodePipelineGithub/ejemplo1/CreadorDeArchivos.java
+++ ejemplo1/src/main/java/practica/mvnEC2CodeDeployCodePipelineGithub/ejemplo1/CreadorDeArchivos.java
@@ -20,30 +20,9 @@ public class CreadorDeArchivos {
 		}
 		else {
 			bw= new BufferedWriter(new FileWriter(archivo));
-			bw.write("esto ya estaba creado, pero seguramente se hizo una modificacion"
-					+"hola otra vez"
+			bw.write("nada mas");
 
-
-
-
-					+ " se agraga mas texto para comprobar algo"
-					+ "ultimas pruebas"
-
-					+ " se agraga mas texto para comprobar algo"
-					+ "otra prueba, pero del alumno2"
-
-
-
-					+ " se agraga mas texto para comprobar algo (modificado por alumno1)"
-					+ "ajjajajajaj"
-					+ "jajajajja"
-					+ "jajajajjaja"
-					+ "ajajjaja"
-					+ "hola mundo"
-					+ "nuevo contenido23"
-					+ "algo mas");
-
-
+		
 		bw.close();
 	}
 	}
