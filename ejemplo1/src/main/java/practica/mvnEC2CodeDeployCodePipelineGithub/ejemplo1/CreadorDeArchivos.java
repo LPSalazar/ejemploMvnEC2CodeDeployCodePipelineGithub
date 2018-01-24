0046b38b4896d20c44bd7dd976d335ad5bdd36c5
LPSalazar <luisp.wtow@gmail.com>
2018-01-24 09:52:15 -0300

    nuevo cambio

--- ejemplo1/src/main/java/practica/mvnEC2CodeDeployCodePipelineGithub/ejemplo1/CreadorDeArchivos.java
+++ ejemplo1/src/main/java/practica/mvnEC2CodeDeployCodePipelineGithub/ejemplo1/CreadorDeArchivos.java
@@ -25,7 +25,8 @@ public class CreadorDeArchivos {
 					+ "ajjajajajaj"
 					+ "jajajajja"
 					+ "jajajajjaja"
-					+ "ajajjaja");
+					+ "ajajjaja"
+					+ "hola mundo");
 		}
 		bw.close();
 	}
